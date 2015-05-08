c60a41edaffd87d7a913b065ce8a0d5c8f742d2c
Gustavo Azevedo <gustavo.m.azevedo@gmail.com>
2015-05-07 23:29:35 -0300

    Full implementation

--- RxFizzBuzz/Program.cs
+++ RxFizzBuzz/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reactive.Linq;
 
 namespace RxFizzBuzz
 {
@@ -9,12 +10,14 @@ namespace RxFizzBuzz
     {
         static void Main(string[] args)
         {
+            Observable.Interval(TimeSpan.FromSeconds(1))
+                .Skip(1) //skips zero
+                .Subscribe(i => Console.WriteLine(i % 3 == 0 && i % 5 == 0 ? "FizzBuzz"
+                                                              : i % 3 == 0 ? "Fizz"
+                                                              : i % 5 == 0 ? "Buzz"
+                                                              : i.ToString()));
 
-
-
-
-
-            Console.WriteLine("Pressione qualquer tecla para terminar...");
+            Console.WriteLine("Press any key to stop...");
             Console.ReadKey();
         }
     }
